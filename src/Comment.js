cff6ee091c301bf41f91f6cac4c5b84b2781ad69
Elliot Chen <lltchen1@gmail.com>
2019-04-10 14:48:44 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,19 @@
-// add Comment component here
+import React, {Component} from "react";
+import 
+
+class X extends Component{
+ render() {
+   return(
+     
+     
+     
+     
+     
+    )}
+}
+
+
+
+
+
+export default Comment  
